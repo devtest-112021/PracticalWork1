ffd5007a5fd539d5dae01c4438ebdc82e88d701e
Home <asda@sss.int>
2022-02-06 22:11:29 +0200

    add

---
+++ src/work32/Task32.java
@@ -0,0 +1,23 @@
+package work32;
+
+import java.util.Scanner;
+
+public class Task32 {
+    public static void main(String[] args) {
+        int  stavka= 12;
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Введите сумму дипозита: ");
+        int summa = scan.nextInt();
+
+        Scanner scan2=new Scanner(System.in);
+        System.out.println("Введите количеcтво месяцев: ");
+        int month = scan2.nextInt();
+
+        double doxod=(summa * stavka * (month*1.0/12))/100;
+
+        System.out.println("Доход за" + " " + month + " " + "месяцев составляет: "+ doxod +" грн" );
+
+    }
+
+}
---
+++ src/work33/Task33.java
@@ -0,0 +1,16 @@
+package work33;
+
+public class Task33 {
+    public static void main(String[] args) {
+        int price=830;
+        int down=13;
+        int up=7;
+        double  resultDownPrice=830-((price*down*1.0)/100);
+        double resultUpPrice=830-(resultDownPrice*up)/100;
+
+
+        System.out.println("Цена после падение на 13%:  "+resultDownPrice+" $");
+        System.out.println("Цена после поднятия на 7%:  "+resultUpPrice+" $");
+    }
+
+}
