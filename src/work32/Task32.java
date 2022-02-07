981c5fc7040439cb1f7a9489502d46be6185b9f7
Home <asda@sss.int>
2022-02-07 18:22:07 +0200

    change

--- src/work32/Task32.java
+++ src/work32/Task32.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Task32 {
     public static void main(String[] args) {
         int  stavka= 12;
-
+        int  monthYear=12;
         Scanner scan=new Scanner(System.in);
         System.out.println("Введите сумму дипозита: ");
         int summa = scan.nextInt();
@@ -14,7 +14,7 @@ public class Task32 {
         System.out.println("Введите количеcтво месяцев: ");
         int month = scan2.nextInt();
 
-        double doxod=(summa * stavka * (month * 1.0 / 12)) / 100;
+        double doxod=(summa * stavka * (month * 1.0 / monthYear)) / 100;
 
         System.out.println("Доход за" + " " + month + " " + "месяцев составляет: "+ doxod +" грн" );
 
